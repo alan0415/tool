af084bcb621cb15593826eca495177333e3860db
alan <ast9501@gmail.com>
2021-04-25 05:51:56 +0000

    Script used to install software

---
+++ script/kubeadm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo touch /etc/apt/sources.list.d/kubernetes.list
+sudo echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 666 /etc/apt/sources.list.d/kubernetes.list
+
+export K_VER="1.19.7-00"
+sudo apt update
+sudo apt-get install -y kubelet=${K_VER}
+sudo apt-get install -y kubectl=${K_VER}
+sudo apt-get install -y kubeadm=${K_VER}
+
+sudo kubeadm config images pull
